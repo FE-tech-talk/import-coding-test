f51fe3286e145416a899e6957701d6ac272f5e7a
codefug <86544979+codefug@users.noreply.github.com>
2025-05-14 22:00:16 +0900

    feat<1주차 이승현>: 3 추가

--- 1/이승현/index.js
+++ 1/이승현/index.js
@@ -8,3 +8,15 @@ function solution1(arr) {
 function solution2(arr) {
   return [...new Set(arr)].sort((a, b) => a - b);
 }
+
+// 2025-05-13
+// 3
+function solution(numbers) {
+  const result = new Set();
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      result.add(numbers[i] + numbers[j]);
+    }
+  }
+  return [...result].sort((a, b) => a - b);
+}
