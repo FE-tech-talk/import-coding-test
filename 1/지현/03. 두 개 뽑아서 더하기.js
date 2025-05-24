f8478e92e3710d108e40196edebbcd33f262ec8d
easyhyun00 <leejihyun0324@gmail.com>
2025-05-24 12:27:28 +0900

    edit: [배열] 두 개 뽑아서 더하기 - 문제 링크 추가

--- 1/지현/03. 두 개 뽑아서 더하기.js
+++ 1/지현/03. 두 개 뽑아서 더하기.js
@@ -1,11 +1,6 @@
 /**
- * 정수 배열 numbers가 주어집니다.
- * numbers에서 서로 다른 인덱스에 있는 2개의 수를 뽑아 더해
- * 만들 수 있는 모든 수를 배열에 오름차순으로 담아 반환하는 solution() 함수를 완성하세요.
- *
- * [제약조건]
- * numbers의 길이는 2 이상 100 이하입니다.
- * numbers의 모든 수는 0 이상 100 이하입니다.
+ * [문제 링크]
+ * https://school.programmers.co.kr/learn/courses/30/lessons/68644
  */
 
 const solution = (input) => {
