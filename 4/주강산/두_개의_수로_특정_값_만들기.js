2b4a5d78e87a9907eeb9652cc84c1edf6d8d7544
JooKangSan <you04045@naver.com>
2025-06-14 01:57:20 +0900

    feat: 두 개의 수로 특정 값 만들기

---
+++ 4/주강산/두_개의_수로_특정_값_만들기.js
@@ -0,0 +1,20 @@
+// n개의 양의 정수로 이루어진 리스트 arr와 정수 target이 주어졌을 때,
+// 이 중에서 합이 target인 두 수가 arr에 있는지 찾고,
+// 있으면 true, 없으면 false를 반환하는 solution() 함수를 작성하세요.
+
+// 제약 조건
+// • n은 2 이상 10,000 이하의 자연수입니다.
+// • arr의 각 원소는 1 이상 10,000 이하의 자연수입니다.
+// • arr의 원소 중 중복되는 원소는 없습니다.
+// • target은 1 이상 20,000 이하의 자연수입니다.
+
+function solution(arr, target){
+  const seen = new Set();
+
+  for (const num of arr) {
+    if (seen.has(target - num)) return true;
+    seen.add(num);
+  }
+
+  return false;
+}
