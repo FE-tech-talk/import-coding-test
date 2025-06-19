ee92a058979301b59316274c5b0034fbba8a58d1
JooKangSan <you04045@naver.com>
2025-06-19 23:48:01 +0900

    feat: 예상 대진표

---
+++ 5/주강산/예상_대진표.js
@@ -0,0 +1,12 @@
+// 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12985
+
+function solution(n,a,b)
+{
+    var answer = 0;
+    while(a != b){
+        a= Math.ceil(a/2);
+        b= Math.ceil(b/2)
+        answer += 1
+    }
+    return answer;
+}
