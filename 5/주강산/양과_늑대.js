c936b1ae44ce4b38431ac440e0e5a24c797f98a9
JooKangSan <you04045@naver.com>
2025-07-05 04:51:15 +0900

    feat<6주차 주강산>: 31추가

---
+++ 5/주강산/양과_늑대.js
@@ -0,0 +1,33 @@
+// 링크: https://school.programmers.co.kr/learn/courses/30/lessons/92343
+
+function solution(info, edges) {
+  const graph = Array.from({ length: info.length }, () => []);
+  for (const [parent, child] of edges) {
+    graph[parent].push(child);
+  }
+
+  let maxSheep = 0;
+
+  function dfs(current, sheep, wolf, nextNodes) {
+    const isSheep = info[current] === 0;
+    sheep += isSheep ? 1 : 0;
+    wolf += isSheep ? 0 : 1;
+
+    if (wolf >= sheep) return;
+
+    maxSheep = Math.max(maxSheep, sheep);
+
+    const next = [...nextNodes];
+    next.push(...graph[current]);
+    const idx = next.indexOf(current);
+    next.splice(idx, 1);
+
+    for (const node of next) {
+      dfs(node, sheep, wolf, next);
+    }
+  }
+
+  dfs(0, 0, 0, [0]);
+
+  return maxSheep;
+}
